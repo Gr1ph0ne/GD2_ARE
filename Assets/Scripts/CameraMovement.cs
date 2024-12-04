6c37ae49ab23a7c317b17d7dc4b64b74b226b7e6
Grigroy Tsibulya <TSIBULYA.GREGORY@GMAIL.COM>
2024-12-04 23:33:35 +0100

    Added Cursor

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,17 +1,35 @@
+using TMPro;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    private Vector3 _circleCenter;
-    private Vector3 _mouseScreenPosition;
-    private Vector3 _mouseWorldPosition;
+    [SerializeField] private GameObject _mainCursorObject;
+    [SerializeField] private GameObject _secondCursorObject;
 
+    private Vector3 _cameraPositon;
+    private Vector2 _cameraLimit;
+    private Vector2 _secondCursorLimit;
+    private Vector2 _circleCenter;
+    private Vector2 _mouseScreenPosition;
+    private Vector2 _mouseWorldPosition;
+    private GameObject _mainCursor;
+    private GameObject _secondCursor;
+    //public Vector2 _cursorHotspot = Vector2.zero;
+
+    private float _cameraHeight;
     private float _circleRadius = 1.5f;
     private float _smoothSpeed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _mainCursor = Instantiate(_mainCursorObject);
+        _secondCursor = Instantiate(_secondCursorObject);
+        _cameraHeight = transform.position.z;
 
+        //GameObject.Instantiate(_mainCursorTexture, Vector3.zero, Quaternion.identity);
+        //GameObject.Instantiate(_secondCursorTexture, Vector3.zero, Quaternion.identity);
+
+        //Cursor.SetCursor(_mainCursorObject, _cursorHotspot, CursorMode.Auto);
     }
 
     // Update is called once per frame
@@ -22,11 +40,25 @@ public class CameraMovement : MonoBehaviour
         _mouseWorldPosition = Camera.main.ScreenToWorldPoint(_mouseScreenPosition);
 
 
-        Vector3 v = _mouseWorldPosition - _circleCenter;
-        v = Vector3.ClampMagnitude(v, _circleRadius);
-        _mouseWorldPosition = _circleCenter + v;
+        _mainCursor.transform.position = _mouseWorldPosition;
 
-        transform.position = Vector3.Lerp(transform.position, _mouseWorldPosition, Time.deltaTime * _smoothSpeed);
+
+        Vector2 playerToMouse = _mouseWorldPosition - _circleCenter;
+        playerToMouse = Vector2.ClampMagnitude(playerToMouse, _circleRadius);
+        _cameraLimit = _circleCenter + playerToMouse;
+
+        _cameraPositon = Vector2.Lerp(transform.position, _cameraLimit, Time.deltaTime * _smoothSpeed);
+        _cameraPositon.z = _cameraHeight;
+        transform.position = _cameraPositon;
+
+
+        Vector2 mouseToPlayer = _circleCenter - _mouseWorldPosition;
+        mouseToPlayer = Vector2.ClampMagnitude(mouseToPlayer, _circleRadius);
+        _secondCursorLimit = _mouseWorldPosition + mouseToPlayer;
+
+        _secondCursor.transform.position = _secondCursorLimit;
+        //_mainCursorObject.transform.localPosition =  _mouseScreenPosition;
+        //_secondCursorObject.transform.localPosition = (_mouseScreenPosition - _circleCenter).normalized;
     }
 
     void OnDrawGizmosSelected()
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,14 +1,31 @@
+﻿using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
     private Rigidbody2D _rigidBody;
 
+    private Vector2 _mouseScreenPosition;
+    private Vector2 _mouseWorldPosition;
+
+    private Vector2 _currentFramePosition;
+    private Vector2 _nextFramePosition;
     private float _horizontal;
     private float _vertical;
-    private float _moveLimiter = 0.7f;
 
+    private float _moveLimiter = 0.7f;
     private float _runSpeed = 10.0f;
+    private float _raycastDistance = 1f;
+
+    private bool _canMove = true;
+
+
+    // Dash
+    private float _dashLength = 5f;
+    private float _dashDuration = 1f;
+    private float _dashSmallCD = 1f;
+    private float _dashLargeCD = 3f;
 
     void Start()
     {
@@ -17,12 +34,28 @@ public class CharacterController : MonoBehaviour
 
     void Update()
     {
-        _horizontal = Input.GetAxisRaw("Horizontal");
-        _vertical = Input.GetAxisRaw("Vertical");
+        _mouseScreenPosition = Input.mousePosition;
+        _mouseWorldPosition = Camera.main.ScreenToWorldPoint(_mouseScreenPosition);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DoDash();
+        }
+        
+    }
+
+    private void DoDash()
+    {
+        _canMove = false;
+        Vector2 dashDirection = (_mouseWorldPosition - _currentFramePosition).normalized;
+        transform.position = Vector2.Lerp(_currentFramePosition, dashDirection * _dashLength, Time.deltaTime * _dashDuration);
+        _canMove = true;
     }
 
     void FixedUpdate()
     {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+
         if (_horizontal != 0 && _vertical != 0) // Check for diagonal movement
         {
             // limit movement speed diagonally, so you move at 70% speed
@@ -30,6 +63,35 @@ public class CharacterController : MonoBehaviour
             _vertical *= _moveLimiter;
         }
 
-        _rigidBody.linearVelocity = new Vector2(_horizontal * _runSpeed, _vertical * _runSpeed);
+        _currentFramePosition = transform.position;
+        _nextFramePosition = _currentFramePosition + new Vector2(_horizontal * _runSpeed, _vertical * _runSpeed);
+        if (_canMove)
+        {
+            _rigidBody.linearVelocity = new Vector2(_horizontal * _runSpeed, _vertical * _runSpeed);
+        }
+        else
+        {
+            _rigidBody.linearVelocity = Vector2.zero;
+        }
+
     }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        //this.transform.position = _nextFramePosition;
+        if (other.CompareTag("Field"))
+        {
+            _canMove = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        //this.transform.position = _nextFramePosition;
+        if (other.CompareTag("Field"))
+        {
+            _canMove = false;
+        }
+    }
+
+
 }
